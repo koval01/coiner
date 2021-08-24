8f106d1886117e07575735822f79d4648907f7f9
yaroslav.koval <koval.developer@gmail.com>
2021-08-24 23:29:27 +0300

    update

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-# group ids or account ids can be retrieved with @username_to_id_bot
-BOT_TOKEN="<bot-token>"
-BOT_OWNER=<bot-owner-id>
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+BOT_OWNER = os.environ.get("BOT_OWNER", None)
+
+DB_HOST = os.environ.get("DB_HOST", None)
+DB_NAME = os.environ.get("DB_NAME", None)
+DB_USER = os.environ.get("DB_USER", None)
+DB_PASS = os.environ.get("DB_PASS", None)
--- handlers/personal_actions.py
+++ handlers/personal_actions.py
@@ -2,3 +2,7 @@ from aiogram import types
 from dispatcher import dp
 import config
 
+
+@dp.message_handler(commands=['balance'])
+async def check_balance(message: types.Message):
+    await message.reply("Твой баланс: %f COINS" % 0.657498786738752)
