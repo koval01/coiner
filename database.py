aaad8a1673747573c342b20a65895a6b0b5d7ccf
Koval Yaroslav <koval@q-writer.com>
2021-08-26 21:46:38 +0300

    Update database.py

--- database.py
+++ database.py
@@ -64,7 +64,7 @@ class PostSQL:
 
     def get_top_balance(self) -> int:
         self.cursor.execute(
-            'select name, balance from wallet order by balance desc limit 25',
+            'select name, balance, user_id from wallet order by balance desc limit 25',
         )
         result = self.cursor.fetchall()
         self.finish()
