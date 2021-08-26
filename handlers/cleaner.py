2d28423f9fcab117f798d08552508c7ff833f7cd
Koval Yaroslav <koval@q-writer.com>
2021-08-26 19:57:27 +0300

    Update cleaner.py

--- handlers/cleaner.py
+++ handlers/cleaner.py
@@ -2,6 +2,7 @@ from aiogram.types.message import Message
 from dispatcher import bot
 import logging
 import database
+import config
 
 
 async def cleaner_body(message: Message) -> None:
@@ -10,12 +11,13 @@ async def cleaner_body(message: Message) -> None:
     :param: Тело сообщения
     :return:
     """
-    try:
-        data = database.PostSQL_ChatManager(message).get_last_message()
-        database.PostSQL_ChatManager(message).modify_last()
-        await bot.delete_message(data[0], data[1])
-    except Exception as e:
-        database.PostSQL_ChatManager(message).add_new_chat()
-        logging.error(e)
+    if config.CLEANER:
+        try:
+            data = database.PostSQL_ChatManager(message).get_last_message()
+            database.PostSQL_ChatManager(message).modify_last()
+            await bot.delete_message(data[0], data[1])
+        except Exception as e:
+            database.PostSQL_ChatManager(message).add_new_chat()
+            logging.error(e)
 
 
