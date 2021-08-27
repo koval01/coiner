f364032cccef93f5504fbb0656eb816a6b287dd1
yaroslav.koval <koval.developer@gmail.com>
2021-08-27 17:24:16 +0300

    update

--- throttling.py
+++ throttling.py
@@ -1,7 +1,7 @@
 from aiogram.utils.exceptions import Throttled
 from aiogram.types.message import Message
 from dispatcher import dp
-from handlers.actions import private_balance_create
+# from handlers.actions import private_balance_create
 
 
 async def throttling_all(msg: Message) -> bool:
@@ -10,6 +10,6 @@ async def throttling_all(msg: Message) -> bool:
     except Throttled:
         return False
     else:
-        await private_balance_create(
-            msg, pass_check=True, cust_usr=msg.from_user.id)
+        # await private_balance_create(
+        #     msg, pass_check=True, cust_usr=msg.from_user.id)
         return True
