42775f979e94e63c9f04bc532b290617bf4cbcee
yaroslav.koval <yaroslav@koval.page>
2022-04-10 14:12:18 +0200

    update inventory functional

--- handlers/actions.py
+++ handlers/actions.py
@@ -143,6 +143,9 @@ async def user_inventory(message: types.Message):
         except:
             inv_user_id = message.from_user.id
         additional_text_inv = None
+        if inv_user_id <= 0:
+            await message.reply("Возможно неверный идентификатор.")
+            return
         if inv_user_id:
             data_user = database.PostSQL().check_user(inv_user_id)
             additional_text_inv = "Вещи <b>%s</b>" % data_user["name"]
