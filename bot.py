3000013b8d41ffa2ebd3980c42831da6a991c72a
yaroslav.koval <yaroslav@koval.page>
2022-04-22 23:45:51 +0200

    refactor SchedulerSlaves

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import logging
 from aiogram import executor
 
 from dispatcher import dp
-from special import slaves_sc
+from special.slaves_sc import SchedulerSlaves
 from special.throttling import ThrottlingMiddleware
 
 import handlers
@@ -13,6 +13,6 @@ import handlers
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     loop = asyncio.get_event_loop()
-    loop.create_task(slaves_sc.scheduler())
+    loop.create_task(SchedulerSlaves().scheduler())
     dp.middleware.setup(ThrottlingMiddleware())
     executor.start_polling(dp, skip_updates=True)
--- special/slaves_sc.py
+++ special/slaves_sc.py
@@ -7,22 +7,25 @@ import database
 from additional.give import init_give
 
 
-async def slaves_() -> None:
-    owners = database.PostSQL().get_slave_owners
-    for i in owners:
-        data = int(database.PostSQL().get_slaves(
-            custom_user=i["user_id"]))
-        add_ = data * config.PAY_PER_SLAVE
-        await init_give(
-            None, sum_=add_, custom_name="рабы",
-            user_=i["user_id"], slaves_mode=True
-        )
+class SchedulerSlaves:
+    def __init__(self) -> None:
+        pass
 
+    async def slaves_(self) -> None:
+        owners = database.PostSQL().get_slave_owners
+        for i in owners:
+            data = int(database.PostSQL().get_slaves(
+                custom_user=i["user_id"]))
+            add_ = data * config.PAY_PER_SLAVE
+            await init_give(
+                None, sum_=add_, custom_name="рабы",
+                user_=i["user_id"], slaves_mode=True
+            )
 
-async def scheduler():
-    # await slaves_()
-    schedule.every().hour.do(slaves_)
+    async def scheduler(self) -> None:
+        # await slaves_()
+        schedule.every().hour.do(self.slaves_)
 
-    while True:
-        await schedule.run_pending()
-        await asyncio.sleep(1)
+        while True:
+            await schedule.run_pending()
+            await asyncio.sleep(1)
